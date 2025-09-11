436eb0484e9c1dc894df7c519b551108f0c99a5d
JSRobertOuimet <jsrobertouimet@gmail.com>
2025-09-10 21:03:48 -0400

    Removed middleware.

--- src/api/case-study/routes/case-study.ts
+++ src/api/case-study/routes/case-study.ts
@@ -4,31 +4,4 @@
 
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter(
-    "api::case-study.case-study",
-    {
-        config: {
-            find: {
-                middlewares: [
-                    async (ctx, next) => {
-                        if (ctx.path.startsWith("/api/case-studies")) {
-                            const headerPassword =
-                                ctx.request.headers["x-page-password"];
-
-                            if (
-                                headerPassword !==
-                                process.env.PAGE_PASSWORD
-                            ) {
-                                return ctx.unauthorized(
-                                    "Invalid or missing password."
-                                );
-                            }
-                        }
-
-                        return next();
-                    },
-                ],
-            },
-        },
-    }
-);
+export default factories.createCoreRouter("api::case-study.case-study");
